37ef61a7b5c979588561072382cafe40a36fec8f
duminda <duminda@Shamini>
2018-11-02 02:28:13 +0800

    Commit-02-11-2018 Changes

---
+++ src/myfirstjavaprograms/MyFirstJavaProgram.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstjavaprograms;
+
+/**
+ *
+ * @author duminda
+ */
+public class MyFirstJavaProgram {
+    
+    /**
+    * main method is the starting point of Java Programs
+    * without main method, cannot run Java programs
+    */ 
+    public static void main(String[] args) {
+        
+        // System.out.println() can use to print any thing
+        System.out.println("Hi Welcome to Java Basic Learnng.....");
+        System.out.println("I'm Duminda Hettiarachchi");
+        System.out.println("-----------------**********------------------");
+    }
+}
